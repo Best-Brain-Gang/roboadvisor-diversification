613870fbab209f21cc76dced13ee1e2a8466fe63
Nathan Patterson <nathan.e.patterson@gmail.com>
2021-10-29 09:40:28 +0900

    add chat bubble bottom right

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,11 @@
 import { Helmet } from 'react-helmet';
-import { Box, Container, Grid } from '@material-ui/core';
+import {
+  Avatar,
+  Box,
+  Container,
+  Grid
+} from '@material-ui/core';
+import ChatBubbleRoundedIcon from '@material-ui/icons/ChatBubbleRounded';
 import Budget from '../components/dashboard/Budget';
 import LatestOrders from '../components/dashboard/LatestOrders';
 import LatestProducts from '../components/dashboard/LatestProducts';
@@ -22,82 +28,45 @@ const Dashboard = () => (
       }}
     >
       <Container maxWidth={false}>
-        <Grid
-          container
-          spacing={3}
-        >
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
+        <Grid container spacing={3}>
+          <Grid item lg={3} sm={6} xl={3} xs={12}>
             <Budget />
           </Grid>
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
+          <Grid item lg={3} sm={6} xl={3} xs={12}>
             <TotalCustomers />
           </Grid>
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
+          <Grid item lg={3} sm={6} xl={3} xs={12}>
             <TasksProgress />
           </Grid>
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xl={3}
-            xs={12}
-          >
+          <Grid item lg={3} sm={6} xl={3} xs={12}>
             <TotalProfit sx={{ height: '100%' }} />
           </Grid>
-          <Grid
-            item
-            lg={8}
-            md={12}
-            xl={9}
-            xs={12}
-          >
+          <Grid item lg={8} md={12} xl={9} xs={12}>
             <Sales />
           </Grid>
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xl={3}
-            xs={12}
-          >
+          <Grid item lg={4} md={6} xl={3} xs={12}>
             <TrafficByDevice sx={{ height: '100%' }} />
           </Grid>
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xl={3}
-            xs={12}
-          >
+          <Grid item lg={4} md={6} xl={3} xs={12}>
             <LatestProducts sx={{ height: '100%' }} />
           </Grid>
-          <Grid
-            item
-            lg={8}
-            md={12}
-            xl={9}
-            xs={12}
-          >
+          <Grid item lg={8} md={12} xl={9} xs={12}>
             <LatestOrders />
           </Grid>
+        </Grid>
+        <Grid item>
+          <Avatar
+            sx={{
+              position: 'absolute',
+              backgroundColor: '#5664d2',
+              height: 56,
+              width: 56,
+              bottom: 10,
+              right: 10
+            }}
+          >
+            <ChatBubbleRoundedIcon />
+          </Avatar>
         </Grid>
       </Container>
     </Box>
