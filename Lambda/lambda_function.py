af02666a6825adc0b3d82a379d304e58c829a75c
Justine Cho <82793625+juzcho@users.noreply.github.com>
2021-11-01 22:25:08 -0700

    fix investment amount

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -16,6 +16,7 @@ def parse_int(n):
     except ValueError:
         return float("nan")
 
+
 def build_validation_result(is_valid, violated_slot, message_content):
     """
     Define a result message structured as Lex response.
@@ -28,6 +29,7 @@ def build_validation_result(is_valid, violated_slot, message_content):
         "violatedSlot": violated_slot,
         "message": {"contentType": "PlainText", "content": message_content},
     }
+
 
 ### Dialog Actions Helper Functions ###
 def get_slots(intent_request):
@@ -53,6 +55,7 @@ def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message)
         },
     }
 
+
 def delegate(session_attributes, slots):
     """
     Defines a delegate slot type response.
@@ -62,6 +65,7 @@ def delegate(session_attributes, slots):
         "sessionAttributes": session_attributes,
         "dialogAction": {"type": "Delegate", "slots": slots},
     }
+
 
 def close(session_attributes, fulfillment_state, message):
     """
@@ -79,12 +83,14 @@ def close(session_attributes, fulfillment_state, message):
 
     return response
 
+
+
 def validate_data(first_name, age, income_amount, debt_amount, investment_amount, risk_level, intent_request):
     
     # The 'firstName' should only contain letters and no numbers or special characters. Creating this validation also changing the slot type to AMAZON.AlphaNumeric instead of AMAZON.US.First_Name
     if first_name is not None:
         if not first_name.isalpha():
-            return build_validation_result(False, 'firstName', "Please type your first name again. This should only consists of letters, and it cannot include numbers and/or special characters.")
+            return build_validation_result(False, 'firstName', "Please provide your first name again. This should only consists of letters, and it cannot include numbers and/or special characters.")
 
     # The `age` should be greater than zero and less than 65.
     if age is not None:
@@ -100,7 +106,7 @@ def validate_data(first_name, age, income_amount, debt_amount, investment_amount
         is_income_amount_valid = income_amount >= 130000
         if not is_income_amount_valid:
             violatedSlot = "incomeAmount"
-            message = "The amount you entered did not qualify for this service. We require a minimum of $130,000 annual income. Please enter a new income amount." 
+            message = "The amount you entered did not qualify for this service. We require a minimum of $130,000 annual income. Please provide a new income amount." 
             return build_validation_result(is_income_amount_valid, violatedSlot, message)
 
     # The `debt_amount` should not be more than 30% of the annual `income_amount`.        
@@ -111,23 +117,24 @@ def validate_data(first_name, age, income_amount, debt_amount, investment_amount
         is_debt_amount_valid = debt_amount <= debt_from_income_amount
         if not is_debt_amount_valid:
             violatedSlot = "debtAmount"
-            message = f"Your debt-to-income ratio is greater than the average. To qualify for this service, your debt needs to be less than {int(debt_percentage*100)}% of your income. Please enter a different amount." 
+            message = f"Your debt-to-income ratio is greater than the average. To qualify for this service, your debt needs to be less than {int(debt_percentage*100)}% of your income. Please provide a different amount." 
             return build_validation_result(is_debt_amount_valid, violatedSlot, message)
 
     # The `investment_amount` should be equal to or greater than 25000.
     if investment_amount is not None:
         investment_amount = parse_int(investment_amount)
-        is_investment_amount_valid = investment_amount >= 25000
+        #income_amount = parse_int(income_amount)
+        is_investment_amount_valid = investment_amount >= 25000 and investment_amount < income_amount
         if not is_investment_amount_valid:
             violatedSlot = "investmentAmount"
-            message = "The amount you entered did not qualify for this service. We require a minimum of $25,000 investment. Please enter a new investment amount." 
+            message = f"The amount you entered did not qualify for this service. We require a minimum of $25,000 investment that is less than your ${int(income_amount)} annual income. Please provide a new investment amount." 
             return build_validation_result(is_investment_amount_valid, violatedSlot, message)
     
     if risk_level is not None:
         risk_level = parse_int(risk_level)
         violatedSlot = "riskLevel"
         if risk_level < 0 or risk_level > 10:
-            return build_validation_result(False, violatedSlot, "Your risk level is invalid, please input your risk between 0 to 10. (0- no risk, 1 to 4- conservative, 5 to 7- moderate, and 8 to 10- aggressive)")
+            return build_validation_result(False, violatedSlot, "Your risk level is invalid, please input your risk between 0 to 10. (0- no risk and 10- high risk)")
     
     return build_validation_result(True, None, None)
 
@@ -135,7 +142,7 @@ def investment_recommendation(risk_level):
     recommendation_result = ''
     risk_level = parse_int(risk_level)
     if risk_level == 0:
-        recommendation_result = 'Your risk level tolerance is zero or no risk.'
+        recommendation_result = 'Your risk level tolerance is no risk.'
     elif risk_level > 0 and risk_level <= 4:
         recommendation_result = 'Your risk level tolerance is conservative.'
     elif risk_level >= 5 and risk_level <= 7:
@@ -143,7 +150,7 @@ def investment_recommendation(risk_level):
     elif risk_level >= 8 and risk_level <= 10:
         recommendation_result = 'Your risk level tolerance is aggressive.'
     else:
-        recommendation_result = 'Invalid input. Please input your risk between 0 to 10. (0- no risk, 1 to 4- conservative, 5 to 7- moderate, and 8 to 10- aggressive)'
+        recommendation_result = 'Invalid input. Please input your risk between 0 to 10. (0- no risk and 10- high risk)'
     return recommendation_result
 
 ### Intents Handlers ###
@@ -160,6 +167,7 @@ def robodiversification_portfolio(intent_request):
     input_risk_level = get_slots(intent_request)["riskLevel"]
     source = intent_request["invocationSource"]
     
+ 
     if source == 'DialogCodeHook':
         slots = get_slots(intent_request)
         validation_result = validate_data(input_first_name, input_age, input_income_amount, input_debt_amount, input_investment_amount, input_risk_level, intent_request)
@@ -194,6 +202,7 @@ def dispatch(intent_request):
         return robodiversification_portfolio(intent_request)
 
     raise Exception("Intent with name " + intent_name + " not supported")
+
 
 ### Main Handler ###
 def lambda_handler(event, context):
